bdc8c5629b69330b4e69ee3c302bcd5a0b604ad6
simeon-s1 <sschmachtenberg@web.de>
2024-03-29 01:34:38 +0100

    fix migration conflict

--- rest_api/migrations/0051_importlog_delete_entereddata_alter_sample_sequence_and_more.py
+++ rest_api/migrations/0051_importlog_delete_entereddata_alter_sample_sequence_and_more.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rest_api', '0049_alter_mutation_alt'),
+        ('rest_api', '0050_alter_sample_sequence'),
     ]
 
     operations = [
