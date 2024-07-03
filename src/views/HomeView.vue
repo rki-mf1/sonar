7cc6ffc46d90c245a3f7794aa4dc35efa4ec555e
YusraSeyam <yusra.seyamy@gmail.com>
2024-07-03 12:56:19 +0200

    detailed info dialog: adjustments & external link to more lineage info

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -50,32 +50,39 @@
             <div style="height: 100%; overflow: auto;">
               <ProgressSpinner size="small" v-if="loading" style="color: whitesmoke" />
               <!-- Dialog for displaying row details -->
-              <Dialog v-model:visible="displayDialogRow" modal header="Sample Details" :style="{ width: '60vw' }">
+              <Dialog v-model:visible="displayDialogRow" modal header="Sequence Details" :style="{ width: '60vw' }">
                 <div v-if="selectedRow">
                   <p v-for="(value, key) in selectedRow" :key="key">
-                    <template v-if="key === 'genomic_profiles'">
-                      <strong>{{ key }}: </strong>
-                      <div style="white-space: normal; word-wrap: break-word;">
-                        <GenomicProfileLabel v-for="(variant, index) in Object.keys(value)"
-                          :variantString="variant" :annotations="value[variant]" :isLast="index === Object.keys(value).length - 1" />
-                      </div>
-                    </template>
-                    <template v-else-if="key === 'proteomic_profiles'">
-                      <strong>{{ key }}: </strong> 
-                      <div style="white-space: normal; word-wrap: break-word;">
-                        <GenomicProfileLabel v-for="(variant, index) in value"
-                          :variantString="variant" :isLast="index === Object.keys(value).length - 1" />
-                      </div>
-                    </template>
-                    <template v-else-if="key === 'properties'">
-                        <strong>{{ key }}:</strong>
-                        <div v-for="item in value" :key="item.name">
-                          <strong>{{ item.name }}:</strong> {{ item.value }}
+                    <p v-if="allColumns.includes(key)">
+                      <template v-if="key === 'genomic_profiles'">
+                        <strong>{{ key }}: </strong>
+                        <div style="white-space: normal; word-wrap: break-word;">
+                          <GenomicProfileLabel v-for="(variant, index) in Object.keys(value)"
+                            :variantString="variant" :annotations="value[variant]" :isLast="index === Object.keys(value).length - 1" />
                         </div>
-                    </template>
-                    <template v-else>
-                      <strong>{{ key }}:</strong> {{ value }}
-                    </template>
+                      </template>
+                      <template v-else-if="key === 'proteomic_profiles'">
+                        <strong>{{ key }}: </strong> 
+                        <div style="white-space: normal; word-wrap: break-word;">
+                          <GenomicProfileLabel v-for="(variant, index) in value"
+                            :variantString="variant" :isLast="index === Object.keys(value).length - 1" />
+                        </div>
+                      </template>
+                      <template v-else-if="key === 'properties'">
+                          <strong>{{ key }}:</strong>
+                          <div v-for="item in value" :key="item.name">
+                            <div v-if="item.name === 'lineage'">
+                              <strong>&nbsp;&nbsp;&nbsp;{{ item.name }}:</strong> {{ item.value }} (<a :href="'https://outbreak.info/situation-reports?pango=' + item.value" target="_blank">more information</a>)
+                            </div>
+                            <div v-else>
+                              <strong>&nbsp;&nbsp;&nbsp;{{ item.name }}:</strong> {{ item.value }}
+                            </div>
+                          </div>
+                      </template>
+                      <template v-else>
+                        <strong>{{ key }}:</strong> {{ value }}
+                      </template>
+                    </p>
                   </p>
                 </div>
               </Dialog>
