037ec80e14a4b64ae0b25fac8218091947c9159b
Huska, Matthew <HuskaM@rki.de>
2024-08-30 14:01:48 +0200

    Update scripts to use the dc-dev.sh, rename some to make naming more consistent

--- scripts/linux/run-celery-tasks.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose -f docker-compose-dev.yml run --rm dev-django celery -A covsonar_backend worker --loglevel=info --without-gossip --without-mingle --without-heartbeat -Ofair
