57ce73635704aa6837bbcbcf5bf0dd150ff9d37d
Huska, Matthew <HuskaM@rki.de>
2024-10-16 12:53:42 +0200

    Add conda env file for sc2 lineages script, reformat with black, add shebang

--- scripts/sc2_lineages/sc2_lineages.py
+++ scripts/sc2_lineages/sc2_lineages.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ## run command: python sc2_lineages.py -o sc2_lineages.tsv
 
 import argparse
@@ -7,27 +8,52 @@ from pango_aliasor.aliasor import Aliasor
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Create file with all pangolin lineages mapped to their direct sublineages.')
-    parser.add_argument('-o', '--output', type=str, help='Path and name of output tsv file.')
+    parser = argparse.ArgumentParser(
+        description="Create file with all pangolin lineages mapped to their direct sublineages."
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, help="Path and name of output tsv file."
+    )
 
     args = parser.parse_args()
     output_file = args.output
 
     path_lineages = "https://raw.githubusercontent.com/cov-lineages/pango-designation/master/lineage_notes.txt"
-    all_lineages = [lineage for lineage in pd.read_csv(path_lineages, sep='\t').iloc[:, 0].tolist()  if not lineage.startswith('*')]
-    
-    aliasor = Aliasor() # If no alias_key.json is passed, downloads the latest version from github # Aliasor('alias_key.json')
-    df = pd.DataFrame({'lineage': all_lineages, 
-                       'parent': ['none' if aliasor.parent(lineage) == '' else aliasor.parent(lineage) for lineage in all_lineages] # e.g: aliasor.parent("BQ.1") # 'BE.1.1.1'
-    }) 
+    all_lineages = [
+        lineage
+        for lineage in pd.read_csv(path_lineages, sep="\t").iloc[:, 0].tolist()
+        if not lineage.startswith("*")
+    ]
 
-    # perform a self-join to get all DIRECT sublineages for each lineage (one per row) 
-    df = pd.merge(df, df, left_on='lineage', right_on='parent', how='left') # join each 'lineage' with matching 'parent' to get each direct sublineage
-    df = df[['lineage_x', 'lineage_y']].rename(columns={'lineage_x': 'lineage', 'lineage_y': 'sublineage'}).dropna(subset=['sublineage'])
+    # If no alias_key.json is passed, downloads the latest version from github # Aliasor('alias_key.json')
+    aliasor = (
+        Aliasor()
+    )  
+    df = pd.DataFrame(
+        {
+            "lineage": all_lineages,
+            "parent": [
+                "none" if aliasor.parent(lineage) == "" else aliasor.parent(lineage)
+                for lineage in all_lineages
+            ],  # e.g: aliasor.parent("BQ.1") # 'BE.1.1.1'
+        }
+    )
 
-    df.to_csv(output_file, sep='\t', index=False)
+    # Perform a self-join to get all DIRECT sublineages for each lineage (one per row)
+    # Join each 'lineage' with matching 'parent' to get each direct sublineage
+    df = pd.merge(
+        df, df, left_on="lineage", right_on="parent", how="left"
+    )  
+    df = (
+        df[["lineage_x", "lineage_y"]]
+        .rename(columns={"lineage_x": "lineage", "lineage_y": "sublineage"})
+        .dropna(subset=["sublineage"])
+    )
+
+    df.to_csv(output_file, sep="\t", index=False)
 
     return 0
+
 
 if __name__ == "__main__":
     main()
