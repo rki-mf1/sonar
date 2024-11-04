55e49a28e7c554c8a09f015c0e87d718ec2cfdf8
Huska, Matthew <HuskaM@rki.de>
2024-11-04 14:49:28 +0100

    First attempt at the VM prod update script (front and backend)

---
+++ scripts/linux/prod-update-back-front.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Building the frontend"
+cd apps/frontend
+conda update -n sonar-frontend nodejs
+conda run -n sonar-frontend npm install
+conda run -n sonar-frontend npm run build-only
+cd -
+
+FRONTEND_DIR=apps/backend/work/frotnend/dist
+echo "Installing the frontend (to $FRONTEND_DIR)"
+mkdir -p "$FRONTEND_DIR"
+rm -r "$FRONTEND_DIR/*"
+cp -r apps/frontend/dist/* "$FRONTEND_DIR"
+
+echo "Rebuilding and restarting the frontend"
+cd apps/backend
+./scripts/linux/dc-prod.sh down --remove-orphans
+./scripts/linux/build-docker-dev.sh
+./scripts/linux/dc-prod.sh up --force-recreate -d
+./scripts/linux/manage-prod.sh migrate
+./scripts/linux/dc-prod.sh restart sonar-django-apscheduler
