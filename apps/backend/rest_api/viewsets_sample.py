67bc61fbce00d9d706f9ad5dc074b06ca9c34e37
Note <kunaphas.kon@gmail.com>
2024-11-11 14:12:05 +0100

    fix: Reorder the comment at the viewsets_sample.py, line 104, to solve SyntaxError: unmatched )

--- apps/backend/rest_api/viewsets_sample.py
+++ apps/backend/rest_api/viewsets_sample.py
@@ -101,10 +101,8 @@ class SampleViewSet(
 
         latest_sample = (
             models.Sample.objects.filter(collection_date__isnull=False)
-            .order_by(
-                "-collection_date"
-            )  # '-' before column name mean "descending order", while without '-' mean "ascending".
-            .first()
+            # '-' before column name mean "descending order", while without '-' mean "ascending".
+            .order_by("-collection_date").first()
         )
         response_dict["latest_sample_date"] = (
             latest_sample.collection_date if latest_sample else None
