7cd8b4065b9421de03b636a946c99ed22793785f
Note <kunaphas.kon@gmail.com>
2024-11-21 17:04:16 +0100

    fix the code that failed in precommit

--- apps/backend/rest_api/viewsets_sample.py
+++ apps/backend/rest_api/viewsets_sample.py
@@ -53,7 +53,6 @@ from .serializers import SampleGenomesSerializerVCF
 from .serializers import SampleSerializer
 
 
-
 @dataclass
 class LineageInfo:
     name: str
@@ -379,13 +378,15 @@ class SampleViewSet(
                     continue
                 datatype = property_to_datatype[property_name]
                 # Determine the exclusion value based on the datatype
-                annotations[f"not_null_count_{property_name}"] = Count(Subquery(
-                    models.Sample.objects.filter(
-                        properties__property__name=property_name,
-                        **{f"properties__{datatype}__isnull": False},
-                        id=OuterRef("id"),
-                    ).values("id")[:1]
-                ))
+                annotations[f"not_null_count_{property_name}"] = Count(
+                    Subquery(
+                        models.Sample.objects.filter(
+                            properties__property__name=property_name,
+                            **{f"properties__{datatype}__isnull": False},
+                            id=OuterRef("id"),
+                        ).values("id")[:1]
+                    )
+                )
             except ValueError as e:
                 LOGGER.error(
                     f"Error with property_name: {property_name}, datatype: {datatype}"
