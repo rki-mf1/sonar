fa39f3b882390792b3dcd74d258456675d2e838c
Simeon Schmachtenberg <simeon.schmachtenberg@futavis.de>
2024-11-21 10:05:05 +0100

    unscope app styles

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -102,13 +102,7 @@ export default {
 </script>
 
 
-<style scoped>
-html,
-body {
-  height: 100%;
-  margin: 0;
-}
-
+<style>
 body {
   width: 100vw;
   margin: -0.5em;
@@ -168,6 +162,15 @@ header {
 /* width */
 ::-webkit-scrollbar {
   width: 5px;
+  height: 5px;
+}
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+::-webkit-scrollbar-thumb {
+  background-color: rgba(155, 155, 155, 0.5);
+  border-radius: 20px;
+  border: transparent;
 }
 
 /* Track */
