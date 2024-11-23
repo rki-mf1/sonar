76c18710b00898a57e18d127cd058f34b0f14810
Huska, Matthew <HuskaM@rki.de>
2024-11-23 22:00:44 +0100

    Run frontend e2e using Firefox only

--- apps/backend/scripts/linux/dc-gh-action.sh
+++ apps/backend/scripts/linux/dc-gh-action.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f compose.yml --env-file conf/docker/common.env --env-file conf/docker/sonar-cli-action.env "$@"
+docker compose -f compose.yml --env-file conf/docker/common.env --env-file conf/docker/sonar-cli-action.env --env-file conf/docker/dev-secrets.env "$@"
