d885e137f985742ab96626587c03fd5ea6fa6ddc
Huska, Matthew <HuskaM@rki.de>
2024-12-09 20:54:32 +0100

    Add basic eslint and prettier setup and npm run commands

---
+++ apps/frontend/eslint.config.js
@@ -0,0 +1,37 @@
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import typescriptEslint from 'typescript-eslint'
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+export default typescriptEslint.config(
+  includeIgnoreFile(gitignorePath),
+  { ignores: ['*.d.ts', '**/coverage', '**/dist'] },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+      ...eslintPluginVue.configs['flat/recommended']
+    ],
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescriptEslint.parser
+      }
+    },
+    rules: {
+      // your rules
+    }
+  },
+  eslintConfigPrettier
+)
