c3aa21e05b42ae6bef58621a484567524dda1120
Juliane Schmachtenberg <jule-schmachtenberg@web.de>
2025-01-23 16:19:09 +0100

    database dump ready test fixed duplicate sequence in seq.fasta.gz

--- apps/cli/tests/test_job.py
+++ apps/cli/tests/test_job.py
@@ -4,17 +4,12 @@ from tests.conftest import run_cli
 def test_get_list_job(capfd, api_url):
     code = run_cli(f" tasks --db {api_url} --list-jobs")
     out, err = capfd.readouterr()
-    # assert "Failed" in out
-    assert "Completed" in out
+    assert "Failed" in out
     assert code == 0
 
 
 def test_get_job_id(capfd, api_url):
-    # code = run_cli(f" tasks --db {api_url} --jobid 'cli_queue-job'")
-    code = run_cli(
-        f" tasks --db {api_url} --jobid 'cli_prop_a6281e0b-f5c0-458e-9783-5f992a0fa03a'"
-    )
+    code = run_cli(f" tasks --db {api_url} --jobid 'cli_queue-job'")
     out, err = capfd.readouterr()
-    # assert "Status: Queued" in out
-    assert "Status: Completed" in out
+    assert "Status: Queued" in out
     assert code == 0
