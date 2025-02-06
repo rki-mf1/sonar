61bf7ca81784484cfe8328a9f9c2b6ccc9a5bb19
YusraSeyam <yusra.seyamy@gmail.com>
2025-02-06 11:13:38 +0100

    handle 'None' case

--- apps/backend/rest_api/viewsets_sample.py
+++ apps/backend/rest_api/viewsets_sample.py
@@ -676,7 +676,10 @@ class SampleViewSet(
 
         # extract lineages up to second dot to form the grouping lineages (e.g. 'BA.2.9' -> 'BA.2')
         lineage_groups = {
-            ".".join(lineage.split(".")[:2]) for lineage in present_lineages
+            [
+                ".".join(lineage.split(".")[:2]) if lineage is not None else None
+                for lineage in present_lineages
+            ]
         }
         lineage_groups_model = models.Lineage.objects.filter(name__in=lineage_groups)
 
