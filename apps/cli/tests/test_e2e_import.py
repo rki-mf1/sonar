eb2621b5408ab961abb35eee0b7f3b6f62e14595
Note Omen <kunaphas.kon@gmail.com>
2025-10-21 00:20:48 +0200

    fix-test: add **arg

--- apps/cli/tests/test_e2e_import.py
+++ apps/cli/tests/test_e2e_import.py
@@ -266,10 +266,10 @@ def test_wfa_anno_no_upload_covid(monkeypatch, api_url, tmpfile_name):
     monkeypatch.setattr(
         "mpire.WorkerPool.imap_unordered",
         lambda self, func, args=(), kwds={}, callback=None, error_callback=None: (
-            func(arg) for arg in args
+            func(**arg) for arg in args
         ),
     )
-    command = f"import --db {api_url} -r MN908947.3 --method 3 --fasta ../../../test-data/sars-cov-2/seqs.fasta.gz --cache {tmpfile_name}/wfa -t 2 --no-upload --auto-anno --must-pass-paranoid --skip-nx"
+    command = f"import --db {api_url} -r MN908947.3 --method 3 --fasta ../../../test-data/sars-cov-2/seqs.fasta.gz --cache {tmpfile_name}/wfa -t 1 --no-upload --auto-anno --must-pass-paranoid --skip-nx"
     code = run_cli(command)
 
     assert code == 0
